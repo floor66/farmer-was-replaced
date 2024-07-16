3349a075325b45b65ac6e9997335f69e872f60ee
F.P.J. den Hartog <fpjdenhartog@gmail.com>
2024-07-16 19:15:45 +0200

    Rename global_queue -> just "queue"

--- bfs.py
+++ bfs.py
@@ -20,30 +20,30 @@ def solve_bfs(graph, current, target_node, visited=None, path=None):
 
 def solve_bfs_it(graph, start, target_node):
 	# Hacky workaround 'cause we cant use classes, so we just use a queue dict!
-	global_queue = {
+	queue = {
 		"enqueue_stack": [],
 		"dequeue_stack": []
 	}
 
 	def enqueue(item):
-		global_queue["enqueue_stack"].append(item)
+		queue["enqueue_stack"].append(item)
 	
 	def dequeue():
-		if not global_queue["dequeue_stack"]:
-			while global_queue["enqueue_stack"]:
-				global_queue["dequeue_stack"].append(global_queue["enqueue_stack"].pop())
+		if not queue["dequeue_stack"]:
+			while queue["enqueue_stack"]:
+				queue["dequeue_stack"].append(queue["enqueue_stack"].pop())
 		
-		if not global_queue["dequeue_stack"]:
+		if not queue["dequeue_stack"]:
 			print("dequeue from empty queue")
 			return foo()
 		
-		return global_queue["dequeue_stack"].pop()
+		return queue["dequeue_stack"].pop()
 	
 	def is_empty():
-		return not global_queue["enqueue_stack"] and not global_queue["dequeue_stack"]
+		return not queue["enqueue_stack"] and not queue["dequeue_stack"]
 	
 	def size():
-		return len(global_queue["enqueue_stack"]) + len(global_queue["dequeue_stack"])
+		return len(queue["enqueue_stack"]) + len(queue["dequeue_stack"])
 
 	enqueue(start)
 	visited = set([start])
